48875cb5190cb88b3d0eb709e8055c3e725baad3
Francisco Mazzoni <fran.mazzoni@gmail.com>
2021-04-17 19:12:33 -0300

    basic question card

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
+import { Question } from "./components/Question";
 
 function App() {
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
-  useEffect(() => {
+
+  const getQuestions = () => {
     const fetchQuestions = async () => {
-      const {
-        data: { results },
-      } = await axios(
-        "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
-      );
-      setQuestions(results);
+      setIsLoading(true);
+      setError(false);
+      try {
+        const {
+          data: { results },
+        } = await axios(
+          "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
+        );
+        //encoder &encode=url3986
+        setQuestions(results);
+        setIsLoading(false);
+      } catch (error) {
+        setError(true);
+      }
     };
     fetchQuestions();
-  }, []);
+  };
 
-  console.log(questions);
+  const gameStarted = !!questions.length > 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!gameStarted && (
+        <>
+          <h1>Welcome to the Trivia Challenge</h1>
+          <div>You will be presented with 10 True or False questions.</div>
+          <div>Can you score 100%</div>
+          <button
+            onClick={() => {
+              getQuestions();
+            }}
+          >
+            {error ? "Reload Questions" : "Start"}
+          </button>
+          {!error && isLoading && <div>Loading Questions</div>}
+        </>
+      )}
+      {gameStarted && <Question data={questions[0]} />}
     </div>
   );
 }
