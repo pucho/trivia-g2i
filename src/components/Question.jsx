805a6793cb252d5f1cd9009a970a987f9d8262ee
Francisco Mazzoni <fran.mazzoni@gmail.com>
2021-04-18 20:25:14 -0300

    unstyled functional trivia

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,15 @@
-export const Question = ({ data }) => {
+export const Question = ({ data, nextQuestion }) => {
   //TODO html parser, sanitize maybe?
   const { category, question } = data;
+
   return (
     <>
       <h1>{category}</h1>
       <div dangerouslySetInnerHTML={{ __html: question }}></div>
+      <div>
+        <button onClick={() => nextQuestion("True")}>True</button>
+        <button onClick={() => nextQuestion("False")}>False</button>
+      </div>
     </>
   );
 };
